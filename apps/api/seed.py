50546eb769fa9cc4933acb8e3f7b717c941af11c
Agari <agareverie@gmail.com>
2026-05-12 15:19:50 +0700

    Created seed.py and added engineering buildings

---
+++ apps/api/seed.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["cu_atlas_db"]
+
+# Drop the initial databases
+db["buildings"].drop()
+
+db["buildings"].insert_many([
+    # ENGINEERING
+    {"code": "ENG1", "name_en": "Engineering 1", "name_th": "ตึก 1", "pronounciation_th": "tuek-nueng", "faculty": "Engineering"},
+    {"code": "ENG2", "name_en": "Engineering 2", "name_th": "ตึก 2", "pronounciation_th": "tuek-song", "faculty": "Engineering"},
+    {"code": "ENG3", "name_en": "Engineering 3", "name_th": "ตึก 3", "pronounciation_th": "tuek-sam", "faculty": "Engineering"},
+    {"code": "ENG4", "name_en": "Engineering 4", "name_th": "ตึก 4", "pronounciation_th": "tuek-si", "faculty": "Engineering"},
+    {"code": "ENG5", "name_en": "Engineering 5", "name_th": "ตึก 5", "pronounciation_th": "tuek-ha", "faculty": "Engineering"},
+    {"code": "EN100", "name_en": "Centennial Memorial Building", "name_th": "ตึก 100 ปี", "pronounciation_th": "tuek-roi-pi", "faculty": "Engineering"},
+    {"code": "HANS", "name_en": "Mechanical Engineering (Hans Building)", "name_th": "ตึกภาควิชาวิศวกรรมเครื่องกล (ฮันส์ บันตลิ)", "pronounciation_th": "tuek-pak-wi-sha-wi-sa-wa-gum-crueng-gon (Hans Bantle)", "faculty": "Engineering"},
+    {"code": "IE", "name_en": "Industrial Engineering", "name_th": "ตึกวิศวกรรมอุตสาหการ", "pronounciation_th": "tuek-wi-sa-wa-gum-oot-sa-ha-garn", "faculty": "Engineering"},
+    {"code": "EE", "name_en": "Electrical Engineering", "name_th": "ตึกวิศวกรรมไฟฟ้า", "pronounciation_th": "tuek-wi-sa-wa-gum-fai-fa", "faculty": "Engineering"},
+    {"code": "EELAB", "name_en": "Electrical Engineering Laboratory", "name_th": "ตึกภาควิชาวิศวกรรมไฟฟ้า", "pronounciation_th": "tuek-pak-wi-sha-wi-sa-wa-gum-fai-fa", "faculty": "Engineering"},
+    {"code": "IELAB", "name_en": "Industrial Engineering Laboratory", "name_th": "ตึกภาควิชาวิศวกรรมอุตสาหการ", "pronounciation_th": "tuek-pak-wi-sha-wi-sa-wa-gum-oot-sa-ha-garn", "faculty": "Engineering"},
+    {"code": "ENV", "name_en": "Environmental Engineering", "name_th": "ตึกวิศวกรรมสิ่งแวดล้อม", "pronounciation_th": "tuek-wi-sa-wa-gum-sing-wadd-lorm", "faculty": "Engineering"},
+    {"code": "MNBLD", "name_en": "Mining and Petroleum Engineering", "name_th": "ตึกภาควิชาวิศวกรรมเหมืองแร่และปิโตรเลียม", "pronounciation_th": "tuek-pak-wi-sha-wi-sa-wa-gum-mueng-rae-lae-petroleum", "faculty": "Engineering"},
+    {"code": "MTBLD", "name_en": "Metallurgical Engineering", "name_th": "ตึกภาควิชาวิศวกรรมโลหการ", "pronounciation_th": "tuek-pak-wi-sha-wi-sa-wa-gum-lo-ha-garn", "faculty": "Engineering"},
+])
+
+client.close()
+print("Database seeded successfully")

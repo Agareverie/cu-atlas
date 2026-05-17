a3c7a54ecb7d0a00fbe43544fbd458521ac549f1
Agari <agareverie@gmail.com>
2026-05-17 14:55:10 +0700

    Allowed CSS to be imported

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,6 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: true,
+});
 
 config.resolver.resolveRequest = (context, moduleName, platform) => {
   if (platform === "web" && moduleName === "react-native-maps") {
